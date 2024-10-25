d5f99f4d633a95196ef3d4801ed5c8d1aa531b1f
beetrbgus <devbeetrb0214@gmail.com>
2024-10-25 23:07:26 +0900

    feat - 같은 경로에서 여러개 할 수 있는 방법
    - next로 요청오는 모든 / 에 대해서 작동하는 방법

--- app.js
+++ app.js
@@ -3,6 +3,17 @@ const express = require('express');
 const app = express();
 
 app.use('/', (req, res, next)=>{
+    console.log('항상 작동함');
+    next(); // next가 있으면 다음 라우트로 전달 됨
+});
+
+
+app.use('/add-product', (req, res, next)=>{
+    console.log('In the add-product!');
+    res.send('<h1>Add Product 페이지!');
+});
+
+app.use('/', (req, res, next)=>{
     console.log('In the middleware!');
     res.send('<h1>Hello from Express!');
 });
