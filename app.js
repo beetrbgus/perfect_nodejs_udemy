1900ed25716eef866a43c59fb15683f978826a57
beetrbgus <devbeetrb0214@gmail.com>
2024-10-25 23:44:27 +0900

    feat - 기본 express
    - form 전송하는 페이지 생성
    - form으로 받은 데이터 req.body는 기본적으로 parse 를 하지 않아 데이터가 들어와도 undefined가 뜸

--- app.js
+++ app.js
@@ -8,12 +8,18 @@ app.use('/', (req, res, next)=>{
 });
 
 
-app.use('/add-product', (req, res, next)=>{
+app.use('/add-product', (req, res, next)=> {
     console.log('In the add-product!');
-    res.send('<h1>Add Product 페이지!');
+    res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit"> Add Product</button></form>');
 });
 
-app.use('/', (req, res, next)=>{
+app.use('/product', (req, res, next)=> { // next는 사용하지 않으면 생략 가능.
+    console.log('redirect product!');
+    console.log(req.body);
+    res.redirect('/');
+});
+
+app.use('/', (req, res, next)=> {
     console.log('In the middleware!');
     res.send('<h1>Hello from Express!');
 });
