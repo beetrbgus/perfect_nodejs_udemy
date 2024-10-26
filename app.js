af8e302287cdfca0ff6b9ffb5243c53a8b676e84
beetrbgus <devbeetrb0214@gmail.com>
2024-10-26 13:05:36 +0900

    feat - 경로 필터림
    - router 앞에 경로를 추가해 공통적인 path 역할을 함

--- app.js
+++ app.js
@@ -7,7 +7,11 @@ const shopRouter =  require('./routes/shop');
 
 app.use(bodyParser.urlencoded({ extended: false})); // body-parser 미들웨어 등록. extended 는 비표준 대상 분석 가능 여부
 
-app.use(adminRouter);
+app.use('/admin', adminRouter);
 app.use(shopRouter);
 
+
+app.use((req, res, next) => {
+    res.status(404).send('<h1>page not found 404 </h1>')
+});
 app.listen(3000); 
