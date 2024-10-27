924db69a0629388d522856c70593ef9303fedb84
beetrbgus <devbeetrb0214@gmail.com>
2024-10-27 21:49:42 +0900

    feat - 정적 파일 제공
    - css 분리
    - express의 static으로 public 내의 파일 제공시킴

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
+const path = require('path');
 
 const adminRouter =  require('./routes/admin');
 const shopRouter =  require('./routes/shop');
@@ -8,8 +9,10 @@ const errorRouter = require('./routes/error');
 
 app.use(bodyParser.urlencoded({ extended: false})); // body-parser 미들웨어 등록. extended 는 비표준 대상 분석 가능 여부
 
+app.use(express.static(path.join(__dirname, 'public')));
+
 app.use('/admin', adminRouter);
-app.use(shopRouter);
+app.use(shopRouter); 
 app.use(errorRouter);
 
 app.listen(3000); 
