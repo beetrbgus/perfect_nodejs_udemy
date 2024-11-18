d6fe995840e4c847f7bc10a4db170f3a7a4e30f8
beetrbgus <devbeetrb0214@gmail.com>
2024-11-18 21:28:04 +0900

    feat - 제품 없을 때 예외 로직 추가

--- models/cart.js
+++ models/cart.js
@@ -45,6 +45,11 @@ module.exports = class Cart {
             }
             const updatedCart = {...JSON.parse(fileContent)};
             const product = updatedCart.products.find(prod => prod.id === id);
+            // 제품이 없으면 삭제 처리하지 않음.
+            if(!product) {
+                return;
+            }
+
             const productQty = product.qty;
             updatedCart.products = updatedCart.products.filter(prod => prod.id !== id);
             
