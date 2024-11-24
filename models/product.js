ea262913b74acbdcc79d74306395cacdcfd5b322
beetrbgus <devbeetrb0214@gmail.com>
2024-11-24 23:04:21 +0900

    feat - sequelize 추가

--- models/product.js
+++ models/product.js
@@ -1,98 +1,30 @@
-// @deprecated 
-// 파일 시스템에서 가져오는 방법 
-// const fs = require('fs');
-// const path = require('path');
-// const p = path.join(
-//     require.main.path,
-//     'data',
-//     'products.json'
-// );
-const Cart = require('./cart');
-const db = require('../util/db_connection');
+const Sequelize = require('sequelize');
 
-const getProductsFromFile = cb => {
+const sequelize = require('../util/db_connection');
 
-    fs.readFile(p, (err, fileContent) => {
-        console.log(err);
-        console.log(fileContent);
-        
-        if(err) {
-            return cb([]);
-        }
-
-        return cb(JSON.parse(fileContent));
-    });
-}
-// module.exports = function Product(){ // ES5 구축자 함수
-
-// }
-
-module.exports = class Product {
-    constructor(id, title, imageUrl, description, price) {
-        this.id = id;
-        this.title = title;
-        this.imageUrl = imageUrl;
-        this.description = description;
-        this.price = price;
+const Product = sequelize.define('product', {
+    id : {
+        type: Sequelize.BIGINT,
+        autoIncrement : true,
+        allowNull : false, 
+        promaryKey : true
+    },
+    title : {
+        type : Sequelize.STRING,
+        allowNull : false, 
+    },
+    price : {
+        type : Sequelize.INTEGER,
+        allowNull : false, 
+    },
+    description : {
+        type : Sequelize.STRING,
+        allowNull : false, 
+    },
+    imageUrl : {
+        type : Sequelize.STRING,
+        allowNull : false,
     }
+});
 
-    save() {
-        return db.execute(
-            `INSERT INTO products(title, price, description, imageUrl) values(?, ? , ? , ?)`
-            , [this.title, this.price, this.description, this.imageUrl]
-        );
-        
-        // getProductsFromFile(products => {
-        //     if(this.id) {
-        //         const existingProductIndex = products.findIndex(prod => prod.id === this.id);
-        //         console.log("existingProductIndex is ", Number(existingProductIndex));
-                
-        //         const updatedProducts = [... products];
-        //         updatedProducts[existingProductIndex] = this;
-
-        //         fs.writeFile(p, JSON.stringify(updatedProducts), (err) => {
-        //             console.log(err);
-        //         });
-        //     } else {
-        //         this.id = Date.now().toString();
-        //         products.push(this);
-        //         fs.writeFile(p, JSON.stringify(products), (err) => {
-        //             console.log(err);
-        //         });
-        //     }
-            
-        // });
-    }
-
-    static fetchAll() {
-        return db.execute('SELECT * FROM products');
-    }
-
-    static findById(id) {
-        return db.execute(`SELECT * FROM products WHERE id = ${id}`);
-    }
-
-    // static findById(id, cb) {
-    //     getProductsFromFile(products => {
-    //         const product = products.find(p=> p.id === id);
-    //         cb(product);
-    //     });
-    // }
-
-    static deleteById(id) {
-        getProductsFromFile(products => {
-            const product = products.find(prod => prod.id === id);
-            const updatedProducts = products.filter(product=> product.id !== id);
-
-            fs.writeFile(p, JSON.stringify(updatedProducts), (err) => {
-                if(!err) {
-                    Cart.deleteProduct(id, product.price);
-                } else {
-                    console.log(err);
-                }
-            });
-            
-        });
-    }
-
-}
+module.exports = Product;
--- util/db_connection.js
+++ util/db_connection.js
@@ -1,10 +1,18 @@
-const mysql = require('mysql2');
+// 일반적인 DB 연결시
+// const mysql = require('mysql2');
+// const connectionPool = mysql.createPool({
+//     host : 'localhost',
+//     user : 'root',
+//     database : 'node-complete',
+//     password : 'root'
+// });
 
-const connectionPool = mysql.createPool({
-    host : 'localhost',
-    user : 'root',
-    database : 'node-complete',
-    password : 'root'
+// Sequelize 사용해 
+const Sequelize = require('sequelize');
+
+const sequelize = new Sequelize('node-complete', 'root', 'root', {
+    dialect : 'mysql',
+    host : 'localhost'
 });
 
-module.exports = connectionPool.promise();
+module.exports = sequelize;
