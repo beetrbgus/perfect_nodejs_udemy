b481e6aec4f662a5a427426a84c924c7f77d9de9
beetrbgus <devbeetrb0214@gmail.com>
2025-04-09 20:51:07 +0900

    feat - typescript 적용용

--- app.js
+++ app.js
@@ -1,57 +1,14 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-
-const app = express();
-const path = require('path');
-
-const adminRoutes =  require('./routes/admin');
-const shopRouter =  require('./routes/shop');
-const sequelize = require('./util/db_connection');
-const Product = require('./models/product');
-const User = require('./models/user');
-
-app.set('view engine', 'pug');
-app.set('view engine', 'ejs');
-
-app.use(bodyParser.urlencoded({ extended: false})); // body-parser 미들웨어 등록. extended 는 비표준 대상 분석 가능 여부
-
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use((req, res, next)=> {
-    User.findByPk(1)
-    .then(user => {
-        req.user = user;
-        next();
-    })
-    .catch(err=> console.log(err));
-});
-
-app.use('/admin', adminRoutes);
-app.use(shopRouter); 
-
-app.use((req, res, next) => {
-    res.status(404).render('404', {pageTitle : "Page Not Founded"}); 
-});
-
-Product.belongsTo(User, { constraints: true, onDelete : 'CASCADE' });
-User.hasMany(Product);
-
-sequelize
-// .sync({force: true})
-.sync()
-.then(result => {
-    return User.findByPk(1);
-})
-.then(user => {
-    if(!user) {
-        return User.create({name: "Max", email : 'test@test.com'});
-    }
-    return user;
-})
-.then(user => {
-    // console.log(user);
-    app.listen(3000); 
-})
-.catch(err => {
-    console.log(err);
+var num1Element = document.getElementById('num1');
+var num2Element = document.getElementById('num2');
+var buttonElement = document.querySelector('button');
+function add(num1, num2) {
+    return num1 + num2;
+}
+console.log(add(1, 2));
+// console.log(add("1","2")); // number 형으로 정의되어 있어서 오류가 남 
+buttonElement === null || buttonElement === void 0 ? void 0 : buttonElement.addEventListener('click', function () {
+    var num1 = num1Element.value;
+    var num2 = num2Element.value;
+    var result = add(+num1, +num2);
+    console.log(result);
 });
--- app.ts
+++ app.ts
@@ -1,3 +1,7 @@
+const num1Element = document.getElementById('num1') as HTMLInputElement;
+const num2Element = document.getElementById('num2') as HTMLInputElement;
+const buttonElement = document.querySelector('button');
+
 function add(num1 : number, num2 : number) {
     return num1 + num2;
 }
@@ -5,3 +9,10 @@ function add(num1 : number, num2 : number) {
 console.log(add(1,2));
 // console.log(add("1","2")); // number 형으로 정의되어 있어서 오류가 남 
 
+buttonElement?.addEventListener('click', () => {
+    const num1 = num1Element.value;
+    const num2 = num2Element.value;
+
+    const result = add(+num1, +num2);
+        console.log(result);
+});
